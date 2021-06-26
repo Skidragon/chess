1378e3b6ef58832154880db8f492261ef83148d3
Simon Davis <simonfreelance1@gmail.com>
2021-06-26 04:54:56 -0400

    Started on creating the Game class

--- src/Game.ts
+++ src/Game.ts
@@ -1,12 +1,24 @@
-type Opponent = 'Player' | 'Computer';
+type OpponentSelection = "Player" | "Computer";
+interface Player {
+  name: string;
+}
+interface Board {
+  setupComputer: (difficulty: "easy" | "medium" | "hard") => void;
+  setupPlayers: (player: Player, player2: Player) => void;
+}
+const player1 = {};
+const player2 = {};
+
 class Game {
-    start() {
-
+  constructor(board: Board) {
+    this.board = board;
+  }
+  start() {}
+  load() {}
+  against(opponent: OpponentSelection): void {
+    if (opponent === "Player") {
     }
-    load() {
-
+    if (opponent === "Computer") {
     }
-    against(opponent: Opponent) {
-
-    }
+  }
 }
